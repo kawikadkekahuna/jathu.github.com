9b74e7517794f50aa20ce8c2c0d327b6b360ebbb
Jathu Satkunarajah <jathu.satkunarajah@gmail.com>
2014-05-20 06:30:17 -0700

    Include play time counter. Style and animation changes.

--- x/pomodoro/pomodoro.js
+++ x/pomodoro/pomodoro.js
@@ -1,5 +1,5 @@
 function Pomodoro() {
-	this.timer = 1500000; //25 minutes -> milliseconds
+	this.timer = 1500000; //25 minutes -> 1500000 milliseconds
 	this.song = "https://www.youtube.com/watch?v=vj5i8s6icsQ"; //Celebration songs
 	this.running = false;
 	this.count = 0;
@@ -28,7 +28,7 @@ Pomodoro.prototype.start = function() {
 	if(!this.running) {
 		var that = this;
 		this.running = true;
-		this.dom.action.innerHTML = "Stop";
+		this.dom.action.innerHTML = "Reset";
 		$('iframe').remove();
 		$('#bar').animate({
 			height: 0
@@ -56,15 +56,20 @@ Pomodoro.prototype.complete = function() {
 	localStorage.setItem('day',(new Date()).getDate());
 	localStorage.setItem('count', this.count);
 	this.setCount();
-	this.reset();
 	this.lockout();
 }
 
 Pomodoro.prototype.lockout = function() {
+	var that = this;
 	$('#done').fadeIn(250, function() {
-		setTimeout(function() {
-			$('#done').fadeOut();
-		}, 300000); //5 Minute rest
+		//5 minutes -> 300000 milliseconds
+		$('#play-time-left').animate({
+			width: 0
+		}, 300000, function() {
+			$('#done').fadeOut(function() {
+				that.reset();
+			});
+		});
 	});
 }
 
