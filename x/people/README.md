87ce171d48bd902833852b6fa3cdaa98c16ed6b7
Jathu Satkunarajah <jathu.satkunarajah@gmail.com>
2014-06-11 06:23:26 -0400

    People.

---
+++ x/people/README.md
@@ -0,0 +1,6 @@
+## People
+
+People who inspire me. The order is random and in no particular order. This list will never be complete.
+
+-----------------------
+June 2014 - Toronto
---
+++ x/people/actions.js
@@ -0,0 +1,46 @@
+function setSize() {
+	var window_x = window.innerWidth;
+	if(window_x < 700) {
+		var size = 3;
+	} else if(window_x >= 700 && window_x < 1000) {
+		var size = 4;
+	} else if(window_x >= 1000 && window_x <= 1500) {
+		var size = 5;
+	} else {
+		var size = 6;
+	}
+	var w = Math.floor(window_x/size);
+	$('img, li').css({
+		width: w,
+		height: w*1.5
+	});
+}
+
+$(window).resize(function() {
+	setSize();
+}).load(function() {
+	$.getJSON('people.json', function(people) {
+		var grid = document.getElementById('grid').children[0], exist = [];
+		while(exist.length < people.length) {
+			var i = Math.floor(Math.random()*people.length);
+			if(exist.indexOf(i) < 0) {
+				exist.push(i);
+				var li = document.createElement('li');
+				li.style.background = "hsl("+(Math.random()*355)+",100%, 60%)";
+				var a = document.createElement('a');
+				a.href = people[i].link;
+				a.target = "_blank";
+				var div = document.createElement('div');
+				var img = document.createElement('img');
+				img.src = "imgs/"+people[i].name.replace(/\s/g,"_").replace(/\W/g,"")+".jpg";
+				div.appendChild(img);
+				a.appendChild(div);
+				li.appendChild(a);
+				grid.appendChild(li);
+			}
+		}
+
+		//Determine size
+		setSize();
+	});	
+});
