2ae6af787d937a8d927690fbb20eacbd741fca12
Jathu Satkunarajah <jathu.satkunarajah@gmail.com>
2015-02-05 21:08:44 -0500

    New design, thanks Sony

---
+++ src/script.js
@@ -0,0 +1,88 @@
+function genGradientVector() {
+	var RANGE = 10;
+	var f = function() {
+		return Math.floor(Math.random() * ((RANGE*2)+1)) - RANGE;
+	}
+	var vec = [f(), f()];
+	return (vec[0] == 0 && vec[1] == 0) ? genGradientVector():vec;
+}
+
+function Ball(ctx, x, y) {
+	var self = this;
+
+	this.init = function(ctx, x, y) {
+		self.ctx = ctx;
+		self.radius = Math.floor(Math.random() * (MAXRADIUS - MINRADIUS) + MINRADIUS);
+		self.x = x || Math.floor(Math.random() * MAXdX);
+		self.y = y || Math.floor(Math.random() * MAXdY);
+		self.color = randomColor({
+			luminosity: "light"
+		});
+		self.gradientVector = genGradientVector();
+	}
+
+	self.draw = function() {
+		self.ctx.beginPath();
+		self.ctx.arc(self.x, self.y, self.radius, 0, 2 * Math.PI, false);
+		self.ctx.fillStyle = self.color;
+		self.ctx.fill();
+		self.x += self.gradientVector[0];
+		self.y += self.gradientVector[1];
+	}
+
+	self.inSpace = function() {
+		return ((self.x >= 0 && self.x <= MAXdX) || (self.y >= 0 && self.y <= MAXdY));
+	}
+
+	this.init(ctx, x, y); // Call initializer
+}
+
+var canvas = document.getElementById('space');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext('2d');
+MAXdX = canvas.offsetWidth;
+MAXdY = canvas.offsetHeight;
+MINRADIUS = 5;
+MAXRADIUS = 15;
+var MYBALLS = [];
+for(var i = 0; i < 100; i++) {
+	MYBALLS.push(new Ball(ctx));
+}
+
+setInterval(function() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	MAXdX = canvas.offsetWidth;
+	MAXdY = canvas.offsetHeight;
+	for(var i = 0; i < MYBALLS.length; i++) {
+		if(MYBALLS[i].inSpace()) {
+			MYBALLS[i].draw();
+		} else {
+			MYBALLS[i] = new Ball(ctx);
+			MYBALLS[i].draw();
+		}
+	}
+}, 30);
+
+function burst(e, amount) {
+	for(var i = 0; i < amount; i++) {
+		MYBALLS.push(new Ball(ctx, e.pageX, e.pageY));
+	}	
+}
+
+CLICKING = false;
+document.addEventListener('mousedown', function(e) {
+	CLICKING = true;
+	burst(e, 50);
+}, false);
+document.addEventListener('mouseup', function(e) {
+	CLICKING = false;
+}, false);
+document.addEventListener('mousemove', function(e) {
+	if(CLICKING) {
+		burst(e, 1);
+	} else if(e.target.tagName.toUpperCase() == "A") {
+		burst(e, 5);
+	}
+}, false);
